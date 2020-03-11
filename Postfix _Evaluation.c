5fd6292147bf2d1b4e63c54b98d96fc17efbf777
HariniJeyaraman <58400069+HariniJeyaraman@users.noreply.github.com>
2020-03-11 21:14:11 +0530

    Update Postfix _Evaluation.c
    Added spaces around operators and added blank line at end of file

--- Postfix _Evaluation.c
+++ Postfix _Evaluation.c
@@ -9,43 +9,43 @@ struct stack
 };
 void push(struct stack *ps,char x)
 {
-    if(ps->Top==99)
+    if(ps->Top == 99)
     printf("\nStack is Full\n");
     else
     {
-      ps->Top=ps->Top+1;
-      ps->A[ps->Top]=x;
+      ps->Top = ps->Top + 1;
+      ps->A[ps->Top] = x;
     }
 }
 
 int evaluate(int op1,int op2,char c)
 {
-   if(c=='+')
-   return op1+op2;
-   else if(c=='-')
-   return op1-op2;
-   else if(c=='*')
-   return op1*op2;
-   else if(c=='/')
+   if(c == '+')
+   return op1 + op2;
+   else if(c == '-')
+   return op1 - op2;
+   else if(c == '*')
+   return op1 * op2;
+   else if(c == '/')
    return op1/op2;
 }
 
 void print(struct stack s)
 {
   int i;
-  for(i=0;i<=s.Top;i++)
+  for(i = 0 ; i <= s.Top ; i++)
   printf("%d\t",s.A[i]);
 }
 
 int pop(struct stack *ps)
 {
    int x;
-   if(ps->Top==-1)
+   if(ps->Top == -1)
    printf("Stack is empty!Element cant be deleted\n");
    else
    {
-     x=ps->A[ps->Top];
-     ps->Top=ps->Top-1;
+     x = ps->A[ps->Top];
+     ps->Top = ps->Top-1;
      return x;
    }
 }
@@ -55,19 +55,19 @@ int main()
   int op1,op2,x,i;
   char optr;
   struct stack s;
-  s.Top=-1;
+  s.Top = -1;
   printf("Enter the postfix exp to be evaluated \n");
   gets(s.A);
-  for(i=0;i<strlen(s.A);i++)
+  for(i = 0 ; i < strlen(s.A) ; i++)
   {
       if(isdigit(s.A[i]))
       push(&s,s.A[i]-'0');
-      else if(s.A[i]=='+'||s.A[i]=='-'||s.A[i]=='*'||s.A[i]=='/')
+      else if(s.A[i] == '+' || s.A[i] == '-' || s.A[i] == '*' || s.A[i] == '/')
       {
-         op1=pop(&s);
-         op2=pop(&s);
-         optr=s.A[i];
-         x=evaluate(op2,op1,optr);
+         op1 = pop(&s);
+         op2 = pop(&s);
+         optr = s.A[i];
+         x = evaluate(op2,op1,optr);
          push(&s,x);
       }
    }
@@ -82,3 +82,5 @@ Enter the postfix exp to be evaluated
 2 3 1 * + 9 -
 The result is :  -4
 */
+
+
